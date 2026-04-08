e9a92647d913a0ef9e5a27255f99f26676efa53a
AlexDrags <47152229+AlexDrags@users.noreply.github.com>
2026-04-08 16:10:18 +0300

    init: deploy version

---
+++ push-handler.js
@@ -0,0 +1,50 @@
+self.addEventListener('install', () => {
+  self.skipWaiting()
+})
+
+self.addEventListener('activate', (event) => {
+  event.waitUntil(self.clients.claim())
+})
+
+self.addEventListener('push', (event) => {
+  console.log('Подписка на событие push...')
+  if (event.data) {
+    try {
+      const pushData = event.data.json()
+      console.log('pushData', pushData)
+      // const notificationOptions = {
+      //   title: pushData.title || 'Новое сообщение',
+      //   body: pushData.body || 'Новое сообщение',
+      //   icon: pushData.icon || '/icon-192x192.png',
+      //   badge: pushData.badge || '/badge-72x72.png',
+      //   image: pushData.image,
+      //   tag: pushData.tag || 'default',
+      //   data: pushData.data,
+      //   requireInteraction: pushData.requireInteraction || false,
+      //   silent: pushData.silent || false,
+      //   vibrate: pushData.vibrate || [200, 100, 200],
+      //   timestamp: Date.now(),
+      //   actions: pushData.actions || [],
+      // }
+      self.registration.showNotification(pushData.title, pushData.body)
+    } catch (error) {
+      console.error('Push notification error:', error)
+    }
+  }
+})
+
+self.addEventListener('notificationclick', (event) => {
+  console.log('Уведомление закрыто')
+  event.notification.close()
+
+  event.waitUntil(
+    self.clients.matchAll({ type: 'window', includeUncontrolled: true }).then((clients) => {
+      clients.forEach((client) => {
+        client.postMessage({
+          type: 'NOTIFICATION_CLICK',
+          data: event.notification.data,
+        })
+      })
+    }),
+  )
+})
