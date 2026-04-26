ef293f7444a7bb5f449b382dd45cdb5345372fb3
AlexDrags <ksander-88@yandex.ru>
2026-04-26 12:40:22 +0300

    feat:add api folder

---
+++ api/getapi.js
@@ -0,0 +1,60 @@
+window.frontgetapi = {
+  call: async function get() {
+    const params = new URL(window.parent.location.href).hash.slice(3).split('|')
+    const nocoKey = params[0]
+    const basicKey = params[1]
+
+    try {
+      console.log('Запрос списка пользователей...')
+      const usersRes = await fetch(
+        'https://alexdrags-ichat-frontapi.ch.daturum.ru/cors/https/nocodb-34be9a.ch.daturum.ru/api/v2/tables/m3amjp4d1rmzzke/records?viewId=vwrnbfvzjhl3o2sa&limit=25&shuffle=0&offset=0&x_allow_headers=Authorization,xc-token',
+        {
+          method: 'GET',
+          headers: {
+            Accept: 'application/json',
+            Authorization: `Basic ${basicKey}`,
+            'xc-token': nocoKey,
+          },
+        },
+      )
+      console.log('Запрос списка сообщений...')
+      const msgRes = await fetch(
+        'https://alexdrags-ichat-frontapi.ch.daturum.ru/cors/https/nocodb-34be9a.ch.daturum.ru/api/v2/tables/moz9mipdwe457yi/records?viewId=vwbtalrea3w83o7x&limit=1000&shuffle=0&offset=0&x_allow_headers=Authorization,xc-token',
+        {
+          method: 'GET',
+          headers: {
+            Accept: 'application/json',
+            Authorization: `Basic ${basicKey}`,
+            'xc-token': nocoKey,
+          },
+        },
+      )
+      console.log('Запрос наличия подписки пользователей...')
+      const subsRes = await fetch(
+        'https://alexdrags-ichat-frontapi.ch.daturum.ru/cors/https/nocodb-34be9a.ch.daturum.ru/api/v2/tables/m483d7vhcnw0hzq/records?viewId=vw899ht6am0vzep5&limit=25&shuffle=0&offset=0&x_allow_headers=Authorization,xc-token',
+        {
+          method: 'GET',
+          headers: {
+            Accept: 'application/json',
+            Authorization: `Basic ${basicKey}`,
+            'xc-token': nocoKey,
+          },
+        },
+      )
+
+      if (!usersRes.ok || !msgRes.ok || !subsRes.ok) {
+        throw new Error(`HTTP ${usersRes.status}, ${msgRes.status}, ${subsRes.status}`)
+      }
+      console.log('Ответ списка пользователей получен, статус:', usersRes.status)
+      const usersData = await usersRes.json()
+      console.log('Ответ списка сообщений получен, статус:', msgRes.status)
+      const msgData = await msgRes.json()
+      console.log('Ответ на наличия подписки пользователей получен, статус:', subsRes.status)
+      const subsData = await subsRes.json()
+
+      return { users: usersData, messages: msgData, subscriptions: subsData }
+    } catch (err) {
+      console.error('Failed to get script:', err)
+    }
+  },
+}
---
+++ api/postapi.js
@@ -0,0 +1,94 @@
+import { buildPushPayload } from '@block65/webcrypto-web-push'
+
+window.frontpostapi = {
+  call: async function post(id, message, img = null, subscriptionData) {
+    const params = new URL(window.parent.location.href).hash.slice(3).split('|')
+    const nocoKey = params[0]
+    const basicKey = params[1]
+    const publicKey = params[2]
+    const privateKey = params[3]
+    // console.log(nocoKey, basicKey, publicKey, privateKey, id, message, img, subscriptionData)
+    try {
+      console.log('Отправка сообщения "', message, '" от пользователя: ', id, '...')
+
+      const sendRes = await fetch(
+        'https://alexdrags-ichat-frontapi.ch.daturum.ru/cors/https/nocodb-34be9a.ch.daturum.ru/api/v2/tables/moz9mipdwe457yi/records?viewId=vwbtalrea3w83o7x&limit=1000&shuffle=0&offset=0&x_allow_headers=Authorization,xc-token,Content-Type&x_http_method_override=POST',
+        {
+          method: 'POST',
+          headers: {
+            'Content-Type': 'application/json',
+            Authorization: `Basic ${basicKey}`,
+            'xc-token': nocoKey,
+          },
+          body: JSON.stringify({
+            user_id: id,
+            text: message,
+            image: img,
+          }),
+        },
+      )
+
+      if (!sendRes.ok) {
+        throw new Error(`HTTP ${sendRes.status}`)
+      }
+
+      {
+        console.log('Отправка push уведомления...', message, subscriptionData)
+
+        const vapid = {
+          subject: 'mailto:your-email@yourdomain.org',
+          publicKey: publicKey,
+          privateKey: privateKey,
+        }
+
+        const subscription = {
+          endpoint: subscriptionData.endpoint,
+          expirationTime: null,
+          keys: {
+            p256dh: subscriptionData.keys.p256dh,
+            auth: subscriptionData.keys.auth,
+          },
+        }
+
+        const jsonMessageData = JSON.stringify({
+          title: 'Новое сообщение:',
+          body: message,
+          icon: '/icons/icon-192x192.png',
+          badge: '/icons/icon-128x128.png',
+          vibrate: [200, 100, 200],
+          data: {
+            url: '/',
+          },
+        })
+
+        const pushMessage = {
+          data: jsonMessageData,
+          options: {
+            ttl: 60,
+          },
+        }
+
+        const payload = await buildPushPayload(pushMessage, subscription, vapid)
+        const prepareEndpoint = subscription.endpoint.replace('https://', '')
+
+        const notificRes = await fetch(
+          `https://alexdrags-ichat-frontapi.ch.daturum.ru/cors/https/${prepareEndpoint}?x_allow_headers=Authorization,Content-Type,Content-Encoding,Encryption,Crypto-Key,TTL`,
+          payload,
+        )
+
+        if (!notificRes.ok) {
+          throw new Error(`HTTP ${notificRes.status}`)
+        }
+
+        console.log('Отправка уведомления выполнена, статус:', notificRes.status)
+      }
+
+      console.log('Отправка сообщени выполнена, статус:', sendRes.status)
+      const data = await sendRes.json()
+      console.log(data)
+      return data
+    } catch (error) {
+      console.error('Failed to post script:', error)
+    }
+  },
+}
