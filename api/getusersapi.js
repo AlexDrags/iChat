5dd273988ec5a1159344fc238252044280292d35
AlexDrags <ksander-88@yandex.ru>
2026-04-28 17:05:18 +0300

    fix: frontapi

--- api/getusersapi.js
+++ api/getusersapi.js
@@ -1,21 +1,12 @@
 window.frontgetusersapi = {
   call: async function get() {
-    // const params = new URL(window.location.href).hash.slice(3).split('|')
-    // const env = localStorage.getItem('__frontapi_env_vars')
-    //   ? JSON.parse(localStorage.getItem('__frontapi_env_vars'))
-    //   : {}
-    const hashSubstring = window.location.hash.substring(1, 249)
-    console.log('hashSubstring:', hashSubstring)
-    const decode = decodeURIComponent(hashSubstring)
-    console.log('decode: ', decode)
-    const env = JSON.parse(decode)
+    const params = new URLSearchParams(window.location.hash.substring(1))
+    const keys = params.get('keys')
+    const envKeys = keys.split('|')
 
-    console.log('env:', env)
-    console.log('Запуск функции getusersapi...', env['keys'])
-    const envArray = env['keys'].split('|')
-    console.log('Полученные параметры:', env)
-    const nocoKey = envArray[0]
-    const basicKey = envArray[1]
+    console.log('envKeys =>', envKeys)
+    const nocoKey = envKeys[0]
+    const basicKey = envKeys[1]
     console.log(nocoKey, basicKey)
     try {
       console.log('Запрос списка пользователей...')
